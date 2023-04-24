c40568d10f3230f79e339ce2ec8ef8236e5183e8
Dmitry <elxf123@gmail.com>
2023-04-24 17:26:41 +0300

    Minor property names refactor + use null-forgiven operator for required properties instead of default values

--- PocketForzaHorizonCommunity.Back/PocketForzaHorizonCommunity.Back.Database/Entities/Cars/CarType.cs
+++ PocketForzaHorizonCommunity.Back/PocketForzaHorizonCommunity.Back.Database/Entities/Cars/CarType.cs
@@ -5,7 +5,7 @@ namespace PocketForzaHorizonCommunity.Back.Database.Entities.CarEntities;
 public class CarType : EntityBase
 {
     [Required]
-    public string Name { get; set; } = string.Empty;
+    public string Name { get; set; } = null!;
 
     public ICollection<Car> Cars { get; set; } = new List<Car>();
 }
