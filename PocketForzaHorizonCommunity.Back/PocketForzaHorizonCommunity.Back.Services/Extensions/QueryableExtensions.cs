ed8b4db854559c878e153b1b8d17531bf0f4eb0a
Dmitry <elxf123@gmail.com>
2023-04-25 23:45:31 +0300

    Add extension to IQueryable interface that allows to get paginated data from initial query

--- PocketForzaHorizonCommunity.Back/PocketForzaHorizonCommunity.Back.Services/Extensions/QueryableExtensions.cs
+++ PocketForzaHorizonCommunity.Back/PocketForzaHorizonCommunity.Back.Services/Extensions/QueryableExtensions.cs
@@ -1,9 +1,26 @@
-﻿namespace PocketForzaHorizonCommunity.Back.Services.Extensions;
+﻿using Microsoft.EntityFrameworkCore;
+using PocketForzaHorizonCommunity.Back.Database;
+using PocketForzaHorizonCommunity.Back.Database.Entities;
+
+namespace PocketForzaHorizonCommunity.Back.Services.Extensions;
 
 public static class QueryableExtensions
 {
-    public static async Task PaginateAsync<TEntity>()
+    public static async Task<PaginationModel<TEntity>> PaginateAsync<TEntity>(this IQueryable<TEntity> query, int page, int pageSize) where TEntity : EntityBase
     {
+        var total = await query.CountAsync();
 
+        var totalPages = (int)Math.Ceiling((double)total / pageSize);
+
+        var result = query.Skip((page - 1) * pageSize).Take(pageSize).ToList();
+
+        return new PaginationModel<TEntity>
+        {
+            Total = total,
+            TotalPages = totalPages,
+            Page = page,
+            PageSize = pageSize,
+            Entities = result,
+        };
     }
 }
