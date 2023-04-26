cb8386f6629e501e67350b17db85e98ea543445a
Dmitry <elxf123@gmail.com>
2023-04-26 16:25:58 +0300

    1) Override get methods for DesignRepo with eager loading for related entities 2) Add DesignService

---
+++ PocketForzaHorizonCommunity.Back/PocketForzaHorizonCommunity.Back.Services/Services/DesignService.cs
@@ -0,0 +1,11 @@
+﻿using PocketForzaHorizonCommunity.Back.Database.Entities.Guides;
+using PocketForzaHorizonCommunity.Back.Database.Repos.Interfaces;
+
+namespace PocketForzaHorizonCommunity.Back.Services.Services;
+
+public class DesignService : ServiceBase<IDesignsRepository, Design>
+{
+    public DesignService(IDesignsRepository repository) : base(repository)
+    {
+    }
+}
