aa486af4217800304b8d99474920d69d771d888d
Dmitry <elxf123@gmail.com>
2023-04-26 16:36:38 +0300

    1) Override get methods for TuneRepo using eager loading for related entities 2) Add TuneService

--- PocketForzaHorizonCommunity.Back/PocketForzaHorizonCommunity.Back.Database/Repos/TunesRepository.cs
+++ PocketForzaHorizonCommunity.Back/PocketForzaHorizonCommunity.Back.Database/Repos/TunesRepository.cs
@@ -1,4 +1,5 @@
-﻿using PocketForzaHorizonCommunity.Back.Database.Entities.Guides;
+﻿using Microsoft.EntityFrameworkCore;
+using PocketForzaHorizonCommunity.Back.Database.Entities.Guides;
 using PocketForzaHorizonCommunity.Back.Database.Repos.Interfaces;
 
 namespace PocketForzaHorizonCommunity.Back.Database.Repos;
@@ -7,5 +8,17 @@ public class TunesRepository : RepositoryBase<Tune>, ITunesRepository
 {
     public TunesRepository(ApplicationDbContext context) : base(context)
     {
+    }
+
+    public override IQueryable<Tune> GetAll() => Context.Set<Tune>().Include(t => t.User).Include(t => t.Car).AsQueryable();
+
+    public override IQueryable<Tune> GetById(Guid id)
+    {
+        return Context.Set<Tune>()
+            .Where(t => t.Id == id)
+            .Include(t => t.Car)
+            .Include(t => t.User)
+            .Include(t => t.TuneOptions)
+            .AsQueryable();
     }
 }
