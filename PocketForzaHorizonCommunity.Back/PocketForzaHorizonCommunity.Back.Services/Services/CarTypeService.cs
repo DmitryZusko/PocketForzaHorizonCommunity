bea33bbcf1a2a0a9bd645f58af1939686b5a1504
Dmitry <elxf123@gmail.com>
2023-04-26 16:02:38 +0300

    Add CarType service

---
+++ PocketForzaHorizonCommunity.Back/PocketForzaHorizonCommunity.Back.Services/Services/CarTypeService.cs
@@ -0,0 +1,11 @@
+﻿using PocketForzaHorizonCommunity.Back.Database.Entities.CarEntities;
+using PocketForzaHorizonCommunity.Back.Database.Repos.Interfaces;
+
+namespace PocketForzaHorizonCommunity.Back.Services.Services;
+
+public class CarTypeService : ServiceBase<ICarTypesRepository, CarType>
+{
+    public CarTypeService(ICarTypesRepository repository) : base(repository)
+    {
+    }
+}
