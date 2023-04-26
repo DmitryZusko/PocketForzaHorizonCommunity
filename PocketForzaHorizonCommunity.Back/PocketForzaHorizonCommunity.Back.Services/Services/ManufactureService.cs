cbbf475154adc6a6262e1d737e435030c1ddde31
Dmitry <elxf123@gmail.com>
2023-04-26 16:05:22 +0300

    Add manufacture service

---
+++ PocketForzaHorizonCommunity.Back/PocketForzaHorizonCommunity.Back.Services/Services/ManufactureService.cs
@@ -0,0 +1,11 @@
+﻿using PocketForzaHorizonCommunity.Back.Database.Entities.CarEntities;
+using PocketForzaHorizonCommunity.Back.Database.Repos.Interfaces;
+
+namespace PocketForzaHorizonCommunity.Back.Services.Services;
+
+public class ManufactureService : ServiceBase<IManufactireRepository, Manufacture>
+{
+    public ManufactureService(IManufactireRepository repository) : base(repository)
+    {
+    }
+}
