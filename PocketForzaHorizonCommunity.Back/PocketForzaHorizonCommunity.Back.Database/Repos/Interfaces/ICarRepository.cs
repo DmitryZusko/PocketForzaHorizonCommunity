a3af9e5270dfb830c0efbba01dc22852c5b5b01e
Dmitry <elxf123@gmail.com>
2023-04-26 18:35:50 +0300

    I don't know why but methods from ICarRepo has been disappeared. I've returned them

--- PocketForzaHorizonCommunity.Back/PocketForzaHorizonCommunity.Back.Database/Repos/Interfaces/ICarRepository.cs
+++ PocketForzaHorizonCommunity.Back/PocketForzaHorizonCommunity.Back.Database/Repos/Interfaces/ICarRepository.cs
@@ -4,5 +4,7 @@ namespace PocketForzaHorizonCommunity.Back.Database.Repos.Interfaces;
 
 public interface ICarRepository : IRepositoryBase<Car>
 {
+    public IQueryable<Car> GetByIdWithTunes(Guid id);
 
+    public IQueryable<Car> GetByIdWithDesigns(Guid id);
 }
