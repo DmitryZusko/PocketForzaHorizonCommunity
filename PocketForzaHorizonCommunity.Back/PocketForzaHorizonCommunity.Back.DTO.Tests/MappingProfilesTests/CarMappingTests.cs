0af3ae34b90b2b08aae4d9f95d5676f49b0803bf
Dmitry <elxf123@gmail.com>
2023-04-27 18:01:10 +0300

    Add tests form car mappings, because they have complex logic

---
+++ PocketForzaHorizonCommunity.Back/PocketForzaHorizonCommunity.Back.DTO.Tests/MappingProfilesTests/CarMappingTests.cs
@@ -0,0 +1,104 @@
+﻿using AutoMapper;
+using NUnit.Framework;
+using PocketForzaHorizonCommunity.Back.Database.Entities.CarEntities;
+using PocketForzaHorizonCommunity.Back.DTO.DTOs.CarDtos;
+using PocketForzaHorizonCommunity.Back.DTO.Mapper;
+using PocketForzaHorizonCommunity.Back.DTO.Requests.Car;
+
+namespace PocketForzaHorizonCommunity.Back.DTO.Tests.MappingProfilesTests;
+
+[TestFixture]
+public class CarMappingTests
+{
+    [Test]
+    public void Car_To_CarDto_Should_Map()
+    {
+        var car = Boilerplate.GetCar();
+        var expected = MapCarToDto(car);
+        var mapper = new MapperConfiguration(cfg => cfg.AddProfile<CarProfile>()).CreateMapper();
+
+        var actual = mapper.Map<Car, CarDto>(car);
+
+        Assert.IsTrue(CompareCars(expected, actual));
+    }
+
+    [Test]
+    public void CreateCarRequest_To_Car_Should_Map()
+    {
+        var request = Boilerplate.GetCreateCarRequest();
+        var expected = Boilerplate.GetCar();
+        var mapper = new MapperConfiguration(cfg => cfg.AddProfile<CarProfile>()).CreateMapper();
+
+        var actual = mapper.Map<CreateCarRequest, Car>(request);
+
+        Assert.IsTrue(CompareCreatedCars(expected, actual));
+    }
+
+    [Test]
+    public void UpdateCarRequest_To_Car_Should_Map()
+    {
+        var request = Boilerplate.GetUpdateCarRequest();
+        var expected = Boilerplate.GetCar();
+        var mapper = new MapperConfiguration(cfg => cfg.AddProfile<CarProfile>()).CreateMapper();
+
+        var actual = mapper.Map<UpdateCarRequest, Car>(request);
+
+        Assert.IsTrue(CompareUpdatedCars(expected, actual));
+    }
+
+    private CarDto MapCarToDto(Car car)
+    {
+        return new CarDto
+        {
+            Id = car.Id.ToString(),
+            Model = car.Model,
+            Year = car.Year,
+            Price = car.Price,
+            Manufacture = car.Manufacture.Name,
+            Type = car.CarType.Name,
+        };
+    }
+
+    private bool? CompareCars(CarDto expected, CarDto actual)
+    {
+        foreach (var property in actual.GetType().GetProperties())
+        {
+            if (property.Name == nameof(expected.Image)) continue;
+            if (!property.GetValue(actual).Equals(property.GetValue(expected))) return false;
+        }
+
+        return true;
+    }
+
+    private bool CompareCreatedCars(Car expected, Car actual)
+    {
+        foreach (var property in expected.GetType().GetProperties())
+        {
+            if (property.Name == nameof(expected.ImagePath)) continue;
+            if (property.Name == nameof(expected.Manufacture)) continue;
+            if (property.Name == nameof(expected.CarType)) continue;
+            if (property.Name == nameof(expected.Tunes)) continue;
+            if (property.Name == nameof(expected.Designs)) continue;
+            if (property.Name == nameof(expected.OwnedCarsByUsers)) continue;
+            if (!property.GetValue(actual).Equals(property.GetValue(expected))) return false;
+        }
+
+        return true;
+    }
+
+    private bool CompareUpdatedCars(Car expected, Car actual)
+    {
+        foreach (var property in expected.GetType().GetProperties())
+        {
+            if (property.Name == nameof(expected.ImagePath)) continue;
+            if (property.Name == nameof(expected.Manufacture)) continue;
+            if (property.Name == nameof(expected.CarType)) continue;
+            if (property.Name == nameof(expected.Tunes)) continue;
+            if (property.Name == nameof(expected.Designs)) continue;
+            if (property.Name == nameof(expected.OwnedCarsByUsers)) continue;
+            if (!property.GetValue(actual).Equals(property.GetValue(expected))) return false;
+        }
+
+        return true;
+    }
+}
