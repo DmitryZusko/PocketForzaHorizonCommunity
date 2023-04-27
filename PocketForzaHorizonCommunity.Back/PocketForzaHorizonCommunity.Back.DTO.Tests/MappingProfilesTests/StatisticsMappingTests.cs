d07d498229da4973a57d3d7a540809421f1ce6a5
Dmitry <elxf123@gmail.com>
2023-04-27 16:22:46 +0300

    Add unit tests for campaign and general statistics to verify mapping config logic

--- PocketForzaHorizonCommunity.Back/PocketForzaHorizonCommunity.Back.DTO.Tests/MappingProfilesTests/StatisticsMappingTests.cs
+++ PocketForzaHorizonCommunity.Back/PocketForzaHorizonCommunity.Back.DTO.Tests/MappingProfilesTests/StatisticsMappingTests.cs
@@ -1,9 +1,8 @@
 ﻿using AutoMapper;
 using NUnit.Framework;
-using PocketForzaHorizonCommunity.Back.Database.Entities;
 using PocketForzaHorizonCommunity.Back.Database.Entities.UserStatistics;
 using PocketForzaHorizonCommunity.Back.DTO.DTOs.StatisticsDtos;
-using PocketForzaHorizonCommunity.Back.Services.Utilities;
+using PocketForzaHorizonCommunity.Back.DTO.Mapper;
 
 namespace PocketForzaHorizonCommunity.Back.DTO.Tests.MappingProfilesTests;
 
@@ -11,21 +10,108 @@ namespace PocketForzaHorizonCommunity.Back.DTO.Tests.MappingProfilesTests;
 public class StatisticsMappingTests
 {
     [Test]
-    public void CampaingStatistics_To_CampaignStatisticsDto_Should_Map(IMapper mapper, IStatisticsGenerator statisticsGenerator)
+    public void CampaingStatistics_To_CampaignStatisticsDto_Should_Map()
     {
-        var user = new ApplicationUser();
+        var statistics = GetCampaignStatisticsSample();
+        var mapper = new MapperConfiguration(cfg => cfg.AddProfile<CampaignStatisticsProfile>()).CreateMapper();
+        var expected = MapCampaignStatisticsToDto(statistics);
 
-        statisticsGenerator.GenerateStatistics(user);
+        var actual = mapper.Map<CampaignStatistics, CampaignStatisticsDto>(statistics);
 
-        var expected = user.CampaignStatistics;
-
-        var actual = mapper.Map<CampaignStatistics, CampaignStatisticsDto>(expected);
-
-        Assert.IsTrue(CompairCampaignStatistics(expected, actual));
-
+        Assert.IsTrue(CompareCampaignStatistics(expected, actual));
     }
 
-    private bool CompairCampaignStatistics(CampaignStatistics expected, CampaignStatisticsDto actual)
+    [Test]
+    public void GeneralStatistics_To_GeneralStatisticsDto_Should_Map()
+    {
+        var statsstics = GetGeneralStatisticsSample();
+        var expected = MapGeneralStatisticsToDto(statsstics);
+        var mapper = new MapperConfiguration(cfg => cfg.AddProfile<GeneralStatisticsProfile>()).CreateMapper();
+
+        var actual = mapper.Map<GeneralStatistics, GeneralStatisticsDto>(statsstics);
+
+        Assert.IsTrue(CompareGeneralstatistics(expected, actual));
+    }
+
+    private CampaignStatistics GetCampaignStatisticsSample()
+    {
+        return new CampaignStatistics
+        {
+            TotalRaces = 10,
+            StoriesCompleted = 10,
+            StoryStarsEarned = 10,
+            HeadToHeadEntered = 10,
+            HeadToHeadWon = 10,
+            ExhibitionsCompleted = 10,
+            SpeedTrapStars = 10,
+            SpeedZoneStars = 10,
+            DangerSignStars = 10,
+            DriftZoneStars = 10,
+            TrailblazerStars = 10,
+        };
+    }
+
+    private GeneralStatistics GetGeneralStatisticsSample()
+    {
+        return new GeneralStatistics
+        {
+            GarageValue = 10,
+            TimeDrivenInTicks = 10_000_000_000,
+            TotalVictories = 10,
+            TotalPodiums = 10,
+            TotalCleanLaps = 10,
+            CollisionsPerRace = 10,
+            DailyChallengesCompleted = 10,
+            WeeklyChallengesComplited = 10,
+            Car = new Database.Entities.CarEntities.Car
+            {
+                Model = "RX-7",
+                Year = 1997,
+                Manufacture = new Database.Entities.CarEntities.Manufacture
+                {
+                    Name = "Mazda",
+                    Country = "Japan",
+                }
+
+            }
+        };
+    }
+
+    private CampaignStatisticsDto MapCampaignStatisticsToDto(CampaignStatistics statistics)
+    {
+        return new CampaignStatisticsDto
+        {
+            TotalRaces = statistics.TotalRaces,
+            StoriesCompleted = statistics.StoriesCompleted,
+            StoryStarsEarned = statistics.StoryStarsEarned,
+            HeadToHeadEntered = statistics.HeadToHeadEntered,
+            HeadToHeadWon = statistics.HeadToHeadWon,
+            ExhibitionsCompleted = statistics.ExhibitionsCompleted,
+            SpeedTrapStars = statistics.SpeedTrapStars,
+            SpeedZoneStars = statistics.SpeedZoneStars,
+            DangerSignStars = statistics.DangerSignStars,
+            DriftZoneStars = statistics.DriftZoneStars,
+            TrailblazerStars = statistics.TrailblazerStars,
+        };
+    }
+
+    private GeneralStatisticsDto MapGeneralStatisticsToDto(GeneralStatistics statistics)
+    {
+        return new GeneralStatisticsDto
+        {
+            GarageValue = statistics.GarageValue,
+            TimeDriven = TimeSpan.FromTicks(statistics.TimeDrivenInTicks),
+            TotalVictories = statistics.TotalVictories,
+            TotalPodiums = statistics.TotalPodiums,
+            TotalCleanLaps = statistics.TotalCleanLaps,
+            CollisionsPerRace = statistics.CollisionsPerRace,
+            DailyChallengesCompleted = statistics.DailyChallengesCompleted,
+            WeeklyChallengesComplited = statistics.WeeklyChallengesComplited,
+            FavouriteCarModel = $"{statistics.Car.Manufacture.Name} {statistics.Car.Model} {statistics.Car.Year}",
+        };
+    }
+
+    private bool CompareCampaignStatistics(CampaignStatisticsDto expected, CampaignStatisticsDto actual)
     {
 
         if (expected.TotalRaces != actual.TotalRaces) return false;
@@ -39,6 +125,21 @@ public class StatisticsMappingTests
         if (expected.DangerSignStars != actual.DangerSignStars) return false;
         if (expected.DriftZoneStars != actual.DriftZoneStars) return false;
         if (expected.TrailblazerStars != actual.TrailblazerStars) return false;
+
+        return true;
+    }
+
+    private bool CompareGeneralstatistics(GeneralStatisticsDto expected, GeneralStatisticsDto actual)
+    {
+        if (expected.GarageValue != actual.GarageValue) return false;
+        if (expected.TimeDriven != actual.TimeDriven) return false;
+        if (expected.TotalVictories != actual.TotalVictories) return false;
+        if (expected.TotalPodiums != actual.TotalPodiums) return false;
+        if (expected.TotalCleanLaps != actual.TotalCleanLaps) return false;
+        if (expected.CollisionsPerRace != actual.CollisionsPerRace) return false;
+        if (expected.DailyChallengesCompleted != actual.DailyChallengesCompleted) return false;
+        if (expected.WeeklyChallengesComplited != actual.WeeklyChallengesComplited) return false;
+        if (expected.FavouriteCarModel != actual.FavouriteCarModel) return false;
 
         return true;
     }
