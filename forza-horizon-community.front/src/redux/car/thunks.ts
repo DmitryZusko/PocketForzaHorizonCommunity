7b3dbf5b99447489c1b6e2da6297703727caf2ee
Dmitry <elxf123@gmail.com>
2023-05-06 18:46:51 +0300

    make changes due to server-side changes

--- forza-horizon-community.front/src/redux/car/thunks.ts
+++ forza-horizon-community.front/src/redux/car/thunks.ts
@@ -1,10 +1,40 @@
-import { IGetCarsRequest } from "@/data-transfer-objects/requests/GetCarsRequest";
+import { IFilteredCarsRequest } from "@/data-transfer-objects/requests/FilteredCarsRequest";
 import carService from "@/services/car-service";
+import customAxios from "@/utilities/custom-axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
 export const getCars = createAsyncThunk(
   "car/getCars",
-  async ({ page, pageSize }: IGetCarsRequest) => {
-    return carService.getCars({ page, pageSize });
+  async (
+    {
+      page,
+      pageSize,
+      minPrice,
+      maxPrice,
+      minYear,
+      maxYear,
+      selectedCountries: selectedCountries,
+      selectedCarTypes: selectedCarTypes,
+      selectedManufactures: selectedManufactures,
+    }: IFilteredCarsRequest,
+    { signal },
+  ) => {
+    const cancelationToken = customAxios.getCancelationToken();
+
+    signal.addEventListener("abort", () => {
+      cancelationToken.cancel();
+    });
+    return carService.getCars({
+      page,
+      pageSize,
+      minPrice,
+      maxPrice,
+      minYear,
+      maxYear,
+      selectedCountries: selectedCountries,
+      selectedCarTypes: selectedCarTypes,
+      selectedManufactures: selectedManufactures,
+      cancelToken: cancelationToken.token,
+    });
   },
 );
