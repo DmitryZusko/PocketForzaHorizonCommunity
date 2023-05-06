ddf6aea0a1da69ccd84487270f1476a534339cef
Dmitry <elxf123@gmail.com>
2023-05-06 18:50:58 +0300

    Add custom slider for filter block

---
+++ forza-horizon-community.front/src/components/FilterCarTablecomponent/components/CustomRangeSlider/CustomRangeSliderComponent.tsx
@@ -0,0 +1,55 @@
+import { Grid, Slider, TextField } from "@mui/material";
+import { ICustomRangeSliderComponentProps } from "../../types";
+import useCustomRangeSliderComponent from "./useCustomRangeSliderComponent";
+
+export default function CustomRangeSliderComponent({
+  validRange,
+  min,
+  max,
+  step,
+  handleRangeChange,
+}: ICustomRangeSliderComponentProps) {
+  const { selectedRange, handleSignleChange, validateMin, validateMax } =
+    useCustomRangeSliderComponent({ min, max, validRange, handleRangeChange });
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={6}>
+        <TextField
+          type="number"
+          value={selectedRange[0] || 0}
+          onChange={handleSignleChange(0)}
+          onBlur={() => validateMin()}
+          inputProps={{
+            step: step,
+            min: min,
+            max: max,
+          }}
+        />
+      </Grid>
+      <Grid item xs={6}>
+        <TextField
+          type="number"
+          value={selectedRange[1] || 0}
+          onChange={handleSignleChange(1)}
+          onBlur={() => validateMax()}
+          inputProps={{
+            min: min,
+            max: max,
+            step: step,
+          }}
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <Slider
+          value={selectedRange}
+          onChange={handleRangeChange}
+          valueLabelDisplay="auto"
+          min={min}
+          max={max}
+          step={step}
+          disableSwap
+        />
+      </Grid>
+    </Grid>
+  );
+}
