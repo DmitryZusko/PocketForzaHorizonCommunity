e72d48d8597da0d3bab3536685f9ed74ccbaed57
Dmitry <elxf123@gmail.com>
2023-05-12 20:08:27 +0300

    Create page for adding new tunes. handleSubmit method will be changed after adding auth

---
+++ forza-horizon-community.front/src/components/forms/AddNewTuneFormComponent/useAddNewTuneFormComponent.ts
@@ -0,0 +1,71 @@
+import { carNamesSelector, getCarNames, useAppDispatch, useAppSelector } from "@/redux";
+import { useFormik } from "formik";
+import { useRouter } from "next/router";
+import { useCallback, useEffect, useMemo } from "react";
+import { initialValues, validationScheme } from "./constants";
+import { IFormikNewTuneValues } from "./types";
+
+export const useAddNewTuneFormComponent = () => {
+  const { isLoadingCarNames, carNames } = useAppSelector(carNamesSelector);
+
+  const dispatch = useAppDispatch();
+  const router = useRouter();
+
+  const handleSubmit = (values: IFormikNewTuneValues) => {
+    console.log(values.title);
+    console.log(values.forzaShareCode);
+    console.log(values.selectedCarId);
+    console.log(values.engineDescription);
+    console.log(values.engine);
+    console.log(values.aspiration);
+    console.log(values.intake);
+    console.log(values.ignition);
+    console.log(values.displacement);
+    console.log(values.exhaust);
+    console.log(values.handlingDescription);
+    console.log(values.brakes);
+    console.log(values.suspension);
+    console.log(values.antiRollBars);
+    console.log(values.rollCage);
+    console.log(values.drivetrainDescription);
+    console.log(values.clutch);
+    console.log(values.transmission);
+    console.log(values.differential);
+    console.log(values.tiresDescription);
+    console.log(values.compound);
+    console.log(values.frontTireWidth);
+    console.log(values.rearTireWidth);
+    console.log(values.frontTrackWidth);
+    console.log(values.rearTrackWidth);
+
+    //cleanInput();
+  };
+
+  const handleCancel = () => {
+    cleanInput();
+    router.push("/tunes");
+  };
+
+  const formik = useFormik({
+    initialValues: initialValues,
+    validationSchema: validationScheme,
+    onSubmit: (values) => handleSubmit(values),
+  });
+
+  const autocompleteOptions = useMemo(() => {
+    return carNames.map((item) => ({
+      label: item.carName,
+      id: item.id,
+    }));
+  }, [carNames]);
+
+  const cleanInput = useCallback(() => {
+    formik.values = initialValues;
+  }, [formik]);
+
+  useEffect(() => {
+    dispatch(getCarNames());
+  }, [dispatch]);
+
+  return { formik, autocompleteOptions, handleCancel };
+};
--- forza-horizon-community.front/src/components/forms/index.ts
+++ forza-horizon-community.front/src/components/forms/index.ts
@@ -1,1 +1,2 @@
 export * from "./AddNewsDesignFormComponent";
+export * from "./AddNewTuneFormComponent";
