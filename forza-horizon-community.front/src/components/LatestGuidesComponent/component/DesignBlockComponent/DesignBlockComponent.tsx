2352d98b0413dc809d8e224f2f695d65329b3e0d
Dmitry <elxf123@gmail.com>
2023-05-15 00:45:14 +0300

    Add styles for a latest designs block

--- forza-horizon-community.front/src/components/LatestGuidesComponent/component/DesignBlockComponent/DesignBlockComponent.tsx
+++ forza-horizon-community.front/src/components/LatestGuidesComponent/component/DesignBlockComponent/DesignBlockComponent.tsx
@@ -1,35 +1,56 @@
-import { GuideCardFooterComponent, NavigationCard } from "@/components";
+import { CardSkeletonComponent, GuideCardFooterComponent, NavigationCard } from "@/components";
+import { CustomTooltipComponent } from "@/components/CustomTooltipComponent";
 import { imageUtil } from "@/utilities";
-import { Grid, Tooltip } from "@mui/material";
+import { Box, Grow, Slide, Typography } from "@mui/material";
+import { styles } from "./styles";
 import useDesignBlockComponent from "./useDesignBlockComponent";
 
 const DesignBlockComponent = ({ ...props }) => {
   const { isLoading, latestDesigns } = useDesignBlockComponent();
 
   return (
-    <Grid container spacing={2} {...props}>
-      {latestDesigns.map((design) => (
-        <Tooltip key={design.id} title="Go to design page">
-          <Grid item key={design.id} xs={12} md={3}>
-            <NavigationCard
-              thumbnail={imageUtil.addJpgHeader(design.thumbnail)}
-              cardTitle={design.title}
-              navigationLink={`designs/${design.id}`}
-              body={design.description}
-              footer={
-                <GuideCardFooterComponent
-                  shareCode={design.forzaShareCode}
-                  rating={design.rating}
-                  author={design.authorUsername}
-                  creationDate={design.creationDate}
-                  carModel={design.carModel}
-                />
-              }
-            />
-          </Grid>
-        </Tooltip>
-      ))}
-    </Grid>
+    <Box sx={styles.outerBox}>
+      <Typography variant="textTitle" align="center" color="primary" sx={styles.headerText}>
+        Newest Designs
+      </Typography>
+
+      <Box sx={styles.cardBox}>
+        {latestDesigns.map((design) =>
+          isLoading ? (
+            <Grow key={design.id} in={isLoading} unmountOnExit>
+              <Box sx={styles.cardBlock}>
+                <CardSkeletonComponent />
+              </Box>
+            </Grow>
+          ) : (
+            <Slide key={design.id} direction="right" in={!isLoading} mountOnEnter timeout={500}>
+              <Box>
+                <CustomTooltipComponent title="Go to Design" sx={styles.cardBlock}>
+                  <Box>
+                    <NavigationCard
+                      thumbnail={imageUtil.addJpgHeader(design.thumbnail)}
+                      cardTitle={design.title}
+                      navigationLink={`designs/${design.id}`}
+                      target={"_self"}
+                      body={<Typography variant="textBody">{design.description}</Typography>}
+                      footer={
+                        <GuideCardFooterComponent
+                          shareCode={design.forzaShareCode}
+                          rating={design.rating}
+                          author={design.authorUsername}
+                          creationDate={design.creationDate}
+                          carModel={design.carModel}
+                        />
+                      }
+                    />
+                  </Box>
+                </CustomTooltipComponent>
+              </Box>
+            </Slide>
+          ),
+        )}
+      </Box>
+    </Box>
   );
 };
 
