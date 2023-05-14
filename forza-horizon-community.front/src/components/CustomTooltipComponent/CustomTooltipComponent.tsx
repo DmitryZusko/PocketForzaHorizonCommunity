d742116c696e57fc30dc24dab2246dc927234c64
Dmitry <elxf123@gmail.com>
2023-05-15 00:43:50 +0300

    Spreading props could be useful

--- forza-horizon-community.front/src/components/CustomTooltipComponent/CustomTooltipComponent.tsx
+++ forza-horizon-community.front/src/components/CustomTooltipComponent/CustomTooltipComponent.tsx
@@ -1,9 +1,9 @@
 import { Tooltip, Typography } from "@mui/material";
 import { ICustomTooltipComponentProps } from "./types";
 
-const CustomTooltipComponent = ({ title, children }: ICustomTooltipComponentProps) => {
+const CustomTooltipComponent = ({ title, children, ...props }: ICustomTooltipComponentProps) => {
   return (
-    <Tooltip arrow title={<Typography variant="tooltip">{title}</Typography>}>
+    <Tooltip arrow title={<Typography variant="tooltip">{title}</Typography>} {...props}>
       {children}
     </Tooltip>
   );
