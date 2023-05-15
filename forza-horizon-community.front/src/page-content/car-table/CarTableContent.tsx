bf78f838e113226982121b51b2570783a4108781
Dmitry <elxf123@gmail.com>
2023-05-16 01:23:57 +0300

    wip branch will becoming handy

--- forza-horizon-community.front/src/page-content/car-table/CarTableContent.tsx
+++ forza-horizon-community.front/src/page-content/car-table/CarTableContent.tsx
@@ -1,9 +1,11 @@
 import { CarTableComponent, FilterCarTableComponent } from "@/components";
+import { NavBarBodyComponent } from "@/components/NavBarComponent/components";
 import { Grid } from "@mui/material";
 
 const CarTableContent = () => {
   return (
     <Grid container>
+      <NavBarBodyComponent />
       <Grid item xs={3}>
         <FilterCarTableComponent />
       </Grid>
