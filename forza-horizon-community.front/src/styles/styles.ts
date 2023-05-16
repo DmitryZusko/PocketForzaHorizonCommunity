8c0a83d08f5cb0fa3730fc5cbf1c887860a8e50a
Dmitry <elxf123@gmail.com>
2023-05-16 13:08:45 +0300

    Desktop filter styles are ready

--- forza-horizon-community.front/src/styles/styles.ts
+++ forza-horizon-community.front/src/styles/styles.ts
@@ -1,9 +1,18 @@
 export const styles = {
   centeredColumnFlexContainer: {
-    margin: "0px",
-    padding: "0px, 0px, 0px, 0px",
+    margin: "0px!important",
+    padding: "0px!important",
     display: "flex",
     flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  centeredRowFlexContainer: {
+    margin: "0px!important",
+    padding: "0px!important",
+    display: "flex",
+    flexDirection: "row",
     justifyContent: "center",
     alignItems: "center",
   },
