149b02baf6173fcf620e2d5b29a0cb9508935a18
Dmitry <elxf123@gmail.com>
2023-05-17 20:06:51 +0300

    Add styles to a tunes page

--- forza-horizon-community.front/src/components/TuneListComponent/useTuneListComponent.ts
+++ forza-horizon-community.front/src/components/TuneListComponent/useTuneListComponent.ts
@@ -1,16 +1,16 @@
 import {
-  addToTunePageSize,
   carNamesSelector,
+  cleanUpTunes,
   getCarNames,
   getTunes,
   getTunesByCarId,
-  setTunePageSize,
+  setTunePage,
   tunesSelector,
+  turnTunePage,
   useAppDispatch,
   useAppSelector,
 } from "@/redux";
 import { useCallback, useEffect, useMemo, useState } from "react";
-import { defaultPageSize } from "../constants";
 
 export const useTuneListComponent = () => {
   const [searchQuery, setSearchQuery] = useState("");
@@ -18,7 +18,7 @@ export const useTuneListComponent = () => {
 
   const { carNames } = useAppSelector(carNamesSelector);
 
-  const { latestTunes, tunes, page, pageSize, totalEntities } = useAppSelector(tunesSelector);
+  const { tunes, page, pageSize, totalEntities } = useAppSelector(tunesSelector);
 
   const dispatch = useAppDispatch();
 
@@ -30,7 +30,7 @@ export const useTuneListComponent = () => {
     if (selectedCar) {
       return dispatch(
         getTunesByCarId({
-          page: 0,
+          page,
           pageSize,
           searchQuery: searchQuery,
           carId: selectedCar,
@@ -45,7 +45,7 @@ export const useTuneListComponent = () => {
         searchQuery: searchQuery,
       }),
     );
-  }, [selectedCar, searchQuery, pageSize, dispatch]);
+  }, [selectedCar, searchQuery, page, pageSize, dispatch]);
 
   const autocompleteOptions = useMemo(() => {
     return carNames.map((item) => ({
@@ -54,24 +54,29 @@ export const useTuneListComponent = () => {
     }));
   }, [carNames]);
 
+  //To clean up old results and start fetching for a new query paramsand, old tunes[] should be cleaned up and page set to 0
   const handleSearchQueryChange = useCallback(
     (newQuery: string) => {
       setSearchQuery(newQuery);
-      dispatch(setTunePageSize(defaultPageSize));
+      dispatch(cleanUpTunes());
+      dispatch(setTunePage(0));
     },
     [dispatch],
   );
 
+  //To clean up old results and start fetching for a new query paramsand, old tunes[] should be cleaned up and page set to 0
   const handleAutocompleteChange = useCallback(
     (event: any, newValue: { label: string; id: string } | null) => {
       setSelectedCar(newValue?.id);
-      dispatch(setTunePageSize(defaultPageSize));
+      dispatch(cleanUpTunes());
+      dispatch(setTunePage(0));
     },
     [dispatch],
   );
 
-  const makePageSizeBigger = useCallback(() => {
-    dispatch(addToTunePageSize());
+  //to trigger further tunes loading we simply change a page
+  const loadNext = useCallback(() => {
+    dispatch(turnTunePage());
   }, [dispatch]);
 
   useEffect(() => {
@@ -81,9 +86,7 @@ export const useTuneListComponent = () => {
   useEffect(() => {
     let isDispatched: boolean;
     var dispatchPromise = loadTunes();
-
     dispatchPromise.then(() => (isDispatched = true));
-
     return () => {
       if (!isDispatched) {
         dispatchPromise.abort();
@@ -91,14 +94,24 @@ export const useTuneListComponent = () => {
     };
   }, [loadTunes]);
 
+  //If user leaves the page and that returns, tunes[] will contain previouse results and a new session will load the same tunes and push it to the old array.
+  //To prevent a such behavior, on a component unmounts tunes[] should be cleaned up
+  useEffect(() => {
+    return () => {
+      dispatch(cleanUpTunes());
+      dispatch(setTunePage(0));
+    };
+  }, [dispatch]);
+
   return {
     tunes,
     searchQuery,
     autocompleteOptions,
+    page,
     pageSize,
     totalEntities,
+    loadNext,
     handleSearchQueryChange,
     handleAutocompleteChange,
-    makePageSizeBigger,
   };
 };
--- forza-horizon-community.front/src/page-content/tune-list/TuneListContent.tsx
+++ forza-horizon-community.front/src/page-content/tune-list/TuneListContent.tsx
@@ -1,7 +1,38 @@
-import { TuneListComponent } from "@/components";
+import {
+  baseTheme,
+  ImageBackgroundComponent,
+  NavBarComponent,
+  PageFooterComponent,
+  TuneListComponent,
+} from "@/components";
+import { globalStyles } from "@/styles";
+import { Box, Container } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import { styles as pageStyles } from "../styles";
 
 const TuneListContent = () => {
-  return <TuneListComponent />;
+  return (
+    <Box minHeight={"100vh"} sx={globalStyles.centeredColumnFlexContainer}>
+      <NavBarComponent />
+      <ImageBackgroundComponent>
+        <Container sx={pageStyles.imageTextBlock}>
+          <Typography variant="imageHeader" align="center">
+            Discover New Car Tunes
+          </Typography>
+          <Typography variant="imageBody" align="center">
+            All tunes are created by{" "}
+            <Box component="div" color={baseTheme.palette.secondary.main}>
+              our community
+            </Box>
+          </Typography>
+        </Container>
+      </ImageBackgroundComponent>
+      <TuneListComponent />
+      <Box sx={pageStyles.footer}>
+        <PageFooterComponent />
+      </Box>
+    </Box>
+  );
 };
 
 export default TuneListContent;
