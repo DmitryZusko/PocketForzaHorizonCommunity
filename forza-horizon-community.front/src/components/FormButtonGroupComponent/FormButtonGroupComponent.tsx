cb3de46e399842fd34ea233c103ed79a1b6cd3e2
Dmitry <elxf123@gmail.com>
2023-05-18 16:38:52 +0300

    refactoring: add an optional handleSubmit param in case using this buttons without a <form> block

--- forza-horizon-community.front/src/components/FormButtonGroupComponent/FormButtonGroupComponent.tsx
+++ forza-horizon-community.front/src/components/FormButtonGroupComponent/FormButtonGroupComponent.tsx
@@ -3,11 +3,15 @@ import { Button, Grid } from "@mui/material";
 import { styles } from "./styles";
 import { IFormButtonGroupComponentProps } from "./types";
 
-const FormButtonGroupComponent = ({ handleCancel, ...props }: IFormButtonGroupComponentProps) => {
+const FormButtonGroupComponent = ({
+  handleCancel,
+  handleSubmit,
+  ...props
+}: IFormButtonGroupComponentProps) => {
   return (
     <Grid container spacing={2} {...props} sx={styles.outerContainer}>
       <Grid item xs={6} sx={globalStyles.centeredRowFlexContainer}>
-        <Button variant="contained" type="submit">
+        <Button variant="contained" type="submit" onClick={handleSubmit}>
           Submit
         </Button>
       </Grid>
