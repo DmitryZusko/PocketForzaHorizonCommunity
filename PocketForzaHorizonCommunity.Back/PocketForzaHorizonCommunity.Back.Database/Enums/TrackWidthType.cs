9f3927213b12cc94ecb1a1cf71bc60a05b6fd472
Dmitry <elxf123@gmail.com>
2023-05-19 20:33:41 +0300

    Now should be OK

---
+++ PocketForzaHorizonCommunity.Back/PocketForzaHorizonCommunity.Back.Database/Enums/TrackWidthType.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace PocketForzaHorizonCommunity.Back.Database.Enums;
+
+[JsonConverter(typeof(StringEnumConverter))]
+public enum TrackWidthType
+{
+    StockTrackWidth,
+    StreetTrackWidth,
+    SportTrackWidth,
+    RaceTrackWidth,
+}
+
+
