8e1ba207a832e6aa430dfe236d2466a53215a314
Dmitry <elxf123@gmail.com>
2023-05-21 14:06:53 +0300

    Add auth enums

--- forza-horizon-community.front/src/components/constants/enums/DifferentialType.ts
+++
@@ -1,8 +0,0 @@
-export enum DifferentialType {
-  StockDifferential = "Stock Diff",
-  SportDifferential = "Sport Diff",
-  RaceDifferential = "Rave Diff",
-  RallyDifferential = "Rally Diff",
-  DriftDifferential = "Drift Diff",
-  OffroadDifferential = "Offroad Diff",
-}
