77233c88bc14bd54c74426cefe518704e25d91d5
Dmitry <elxf123@gmail.com>
2023-05-21 15:16:22 +0300

    Add user roles prop to a back-end user dto

--- PocketForzaHorizonCommunity.Back/PocketForzaHorizonComunity.Back.DTO/DTOs/Auth/UserDto.cs
+++ PocketForzaHorizonCommunity.Back/PocketForzaHorizonComunity.Back.DTO/DTOs/Auth/UserDto.cs
@@ -7,6 +7,7 @@ public class UserDto
     public string Id { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public string Username { get; set; } = string.Empty;
+    public List<string> Roles { get; set; } = new List<string>();
     public GeneralStatisticsDto GeneralStatistics { get; set; } = null!;
     public CampaignStatisticsDto CampaignStatistics { get; set; } = null!;
     public OnlineStatisticsDto OnlineStatistics { get; set; } = null!;
