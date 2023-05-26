82c5ece49bf7e96d2ada9c4b41185ec1038c7f7c
Dmitry <elxf123@gmail.com>
2023-05-26 15:06:06 +0300

    bugfix: password/username forms should not allow whitespaces

--- forza-horizon-community.front/src/components/forms/ResetPasswordFormComponent/constants.ts
+++ forza-horizon-community.front/src/components/forms/ResetPasswordFormComponent/constants.ts
@@ -8,6 +8,7 @@ export const validationScheme = Yup.object({
     .matches(/[a-z]/g, "Password should contain a lowercase letter")
     .matches(/[A-Z]/g, "Password should contain an uppercase letter")
     .matches(/\W/g, "Password should contain a symbol")
+    .matches(/^\S*$/g, "Password shouldn't contain whitespaces")
     .required("Please, enter a password"),
   confirmPassword: Yup.string().oneOf([Yup.ref("newPassword")], "Passwords should match"),
 });
